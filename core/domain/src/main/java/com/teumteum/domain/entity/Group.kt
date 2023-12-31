5b2ab340e4ed6cf40db8c1fd84a898eae1d45ac1
Leekangmin <kkk5474096@naver.com>
2023-12-31 16:26:03 +0900

    [feature/#5] Group domain

---
+++ core/domain/src/main/java/com/teumteum/domain/entity/Group.kt
@@ -0,0 +1,10 @@
+package com.teumteum.domain.entity
+
+data class Group(
+    val id: Long,
+    val topic: String,
+    val name: String,
+    val introduction: String,
+    val photoUrls: List<String>,
+    val date: String
+)
