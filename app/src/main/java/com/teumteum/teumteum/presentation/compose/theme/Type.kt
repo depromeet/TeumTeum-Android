d2d4e8f118822088818bfd4e0a5b396702316a4e
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-01-08 00:45:02 +0900

    [fix/#24] font res 위치 변경

--- app/src/main/java/com/teumteum/teumteum/presentation/compose/theme/Type.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/compose/theme/Type.kt
@@ -10,17 +10,17 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.teumteum.teumteum.R.font
+import com.teumteum.base.R
 
 
 val Pretendard = FontFamily(
-    Font(font.pretendard_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
-    Font(font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
-    Font(font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal),
-    Font(font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
-    Font(font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
-    Font(font.pretendard_light, FontWeight.Light, FontStyle.Normal),
-    Font(font.pretendard_extralight, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight, FontStyle.Normal),
 )
 
 // Set of Material typography styles to start with
