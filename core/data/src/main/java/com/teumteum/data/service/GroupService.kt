a33db6d345f53f7b71325d42466896accdf7fd34
Leekangmin <kkk5474096@naver.com>
2024-01-11 16:26:54 +0900

    [feat/#11] RemoteGroupDataSource

--- core/data/src/main/java/com/teumteum/data/service/GroupService.kt
+++ core/data/src/main/java/com/teumteum/data/service/GroupService.kt
@@ -1,6 +1,5 @@
 package com.teumteum.data.service
 
-import com.teumteum.data.model.response.ResponseGetRecommendMeet
 import com.teumteum.data.model.response.ResponseGroup
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,9 +7,9 @@ import retrofit2.http.Query
 interface GroupService {
     @GET("meetings")
     suspend fun getGroups(
-        @Query("size") size: Int,
+        @Query("size") size: Int = 20,
         @Query("page") page: Int,
-        @Query("sort") sort: String,
+        @Query("sort") sort: String = "id,desc",
         @Query("isOpen") isOpen: Boolean,
         @Query("topic") topic: String?,
         @Query("meetingAreaStreet") meetingAreaStreet: String?,
