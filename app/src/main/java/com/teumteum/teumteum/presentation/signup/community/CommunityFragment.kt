77102ae00fd984363d47b826e99611a8b32ce893
blueme0 <ksytpdus0304@gmail.com>
2024-01-15 22:40:04 +0900

    [fix/#35] 리뷰 반영

--- app/src/main/java/com/teumteum/teumteum/presentation/signup/community/CommunityFragment.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/signup/community/CommunityFragment.kt
@@ -9,6 +9,7 @@ import com.teumteum.teumteum.R
 import com.teumteum.teumteum.databinding.FragmentCommunityBinding
 import com.teumteum.teumteum.presentation.signup.SignUpActivity
 import com.teumteum.teumteum.presentation.signup.SignUpViewModel
+import com.teumteum.teumteum.presentation.signup.job.CurrentJobFragment.Companion.JOB_PLANNING
 import com.teumteum.teumteum.presentation.signup.modal.SingleModalBottomSheet
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -19,7 +20,6 @@ class CommunityFragment
 
     private val viewModel by activityViewModels<SignUpViewModel>()
     private var bottomSheet: SingleModalBottomSheet? = null
-    private val communityList = ArrayList<String>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,14 +31,12 @@ class CommunityFragment
     }
 
     private fun initBottomSheet() {
-        communityList.addAll(resources.getStringArray(R.array.community_status))
-
         val listener: (String) -> Unit = { item ->
             viewModel.updateCommunity(item)
             bottomSheet?.dismiss()
         }
 
-        bottomSheet = SingleModalBottomSheet.newInstance("상태 입력", communityList, listener)
+        bottomSheet = SingleModalBottomSheet.newInstance("상태 입력", communitySort, listener)
 
         with(binding) {
             llStatus.setOnClickListener {
@@ -53,13 +51,18 @@ class CommunityFragment
     private fun checkValidInput() {
         lifecycleScope.launch {
             viewModel.community.collect { community ->
-                if (community in communityList) (activity as SignUpActivity).activateNextButton()
+                if (community in communitySort) (activity as SignUpActivity).activateNextButton()
                 else (activity as SignUpActivity).disableNextButton()
             }
         }
     }
 
     companion object {
+        private const val COMMUNITY_WORKER = "직장인"
+        private const val COMMUNITY_STUDENT = "학생"
+        private const val COMMUNITY_TRAINEE = "취업준비생"
+
+        val communitySort = arrayListOf(COMMUNITY_WORKER, COMMUNITY_STUDENT, COMMUNITY_TRAINEE)
     }
 
 }
