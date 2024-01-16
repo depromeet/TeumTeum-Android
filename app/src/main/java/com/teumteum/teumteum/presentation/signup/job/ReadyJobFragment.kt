5b2b5cf6d1054d57c3640465399490c0cdffbac5
blueme0 <ksytpdus0304@gmail.com>
2024-01-17 05:10:20 +0900

    [fix/#37] 코드 리팩토링 및 직장인 시점 직업 선택 화면 오류 수정

--- app/src/main/java/com/teumteum/teumteum/presentation/signup/job/ReadyJobFragment.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/signup/job/ReadyJobFragment.kt
@@ -1,8 +1,6 @@
 package com.teumteum.teumteum.presentation.signup.job
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -22,10 +20,6 @@ class ReadyJobFragment
     private val viewModel by activityViewModels<SignUpViewModel>()
     private var jobClassBottomSheet: SingleModalBottomSheet? = null
     private var jobDetailClassBottomSheet: SingleModalBottomSheet? = null
-    private val jobSort = ArrayList<String>()
-    private val jobDesigner = ArrayList<String>()
-    private val jobDev = ArrayList<String>()
-    private val jobManager = ArrayList<String>()
     private var jobDetailList = ArrayList<String>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,17 +27,8 @@ class ReadyJobFragment
 
         binding.vm = viewModel
         binding.lifecycleOwner = this
-        initJobInfo()
         initBottomSheet()
         checkValidInput()
-    }
-
-    private fun initJobInfo() {
-        jobSort.addAll(arrayOf("디자인", "개발", "기획"))
-        jobDesigner.addAll(arrayOf("프로덕트 디자이너", "BX 디자이너", "그래픽 디자이너",
-            "영상 디자이너", "UX 디자이너", "UI 디자이너", "플랫폼 디자이너"))
-        jobDev.addAll(arrayOf("BE 개발자", "iOS 개발자", "AOS 개발자", "FE 개발자"))
-        jobManager.addAll(arrayOf("PO", "PM", "서비스 기획자"))
     }
 
     private fun initBottomSheet() {
@@ -62,8 +47,11 @@ class ReadyJobFragment
 
         with(binding) {
             llWho.setOnClickListener {
-                jobClassBottomSheet?.setFocusedImageView(ivShowWho)
-                jobClassBottomSheet?.setSelectedItem(viewModel.readyJobClass.value)
+                jobClassBottomSheet?.apply {
+                    setFocusedImageView(ivShowWho)
+                    setSelectedItem(viewModel.readyJobClass.value)
+
+                }
                 jobClassBottomSheet?.show(childFragmentManager, SingleModalBottomSheet.TAG)
                 ivShowWho.setImageResource(R.drawable.ic_arrow_up_l)
             }
@@ -71,15 +59,17 @@ class ReadyJobFragment
             llWhat.setOnClickListener {
                 if (viewModel.readyJobClass.value in jobSort) {
                     jobDetailList = when (viewModel.readyJobClass.value) {
-                        "디자인" -> jobDesigner
-                        "개발" -> jobDev
-                        "기획" -> jobManager
+                        JOB_DESIGN -> jobDesigner
+                        JOB_DEVELOPMENT -> jobDev
+                        JOB_PLANNING -> jobManager
                         else -> ArrayList()
                     }
                     if (viewModel.readyJobClass.value in jobSort) {
-                        jobDetailClassBottomSheet = SingleModalBottomSheet.newInstance("직군 입력", jobDetailList, jobDetailClassListener)
-                        jobDetailClassBottomSheet?.setFocusedImageView(ivShowWhat)
-                        jobDetailClassBottomSheet?.setSelectedItem(viewModel.readyJobDetailClass.value)
+                        jobDetailClassBottomSheet = SingleModalBottomSheet.newInstance("직무 입력", jobDetailList, jobDetailClassListener)
+                        jobDetailClassBottomSheet?.apply {
+                            setFocusedImageView(ivShowWhat)
+                            setSelectedItem(viewModel.readyJobDetailClass.value)
+                        }
                         jobDetailClassBottomSheet?.show(childFragmentManager, SingleModalBottomSheet.TAG)
                         ivShowWhat.setImageResource(R.drawable.ic_arrow_up_l)
                     }
@@ -98,5 +88,23 @@ class ReadyJobFragment
     }
 
     companion object {
+        const val JOB_DESIGN = "디자인"
+        const val JOB_DEVELOPMENT = "개발"
+        const val JOB_PLANNING = "기획"
+
+        val jobSort = arrayListOf(JOB_DESIGN, JOB_DEVELOPMENT, JOB_PLANNING)
+
+        val jobDesigner = arrayListOf(
+            "프로덕트 디자이너", "BX 디자이너", "그래픽 디자이너",
+            "영상 디자이너", "UX 디자이너", "UI 디자이너", "플랫폼 디자이너"
+        )
+
+        val jobDev = arrayListOf(
+            "BE 개발자", "iOS 개발자", "AOS 개발자", "FE 개발자"
+        )
+
+        val jobManager = arrayListOf(
+            "PO", "PM", "서비스 기획자"
+        )
     }
 }
