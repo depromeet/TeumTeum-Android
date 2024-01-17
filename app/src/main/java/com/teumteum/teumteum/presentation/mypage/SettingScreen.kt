dcedb8a94a8470ba39f654faa7997c85fd01701a
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-01-17 20:01:54 +0900

    [feat/#45] Scaffold 수정(Setting 겸용)

--- app/src/main/java/com/teumteum/teumteum/presentation/mypage/SettingScreen.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/mypage/SettingScreen.kt
@@ -1,8 +1,15 @@
 package com.teumteum.teumteum.presentation.mypage
 
 import androidx.compose.runtime.Composable
+import com.teumteum.base.component.compose.TmScaffold
 
 @Composable
 fun SettingScreen() {
+    TmScaffold(
+        isSetting = true,
+        topbarText = "정은아님의 소개서"
+    ) {
 
+
+    }
 }
