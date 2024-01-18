c909288d550252d0c3cc1a53dc4b9cd597a20d73
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-01-18 20:55:58 +0900

    [feat/#45] 회원 탈퇴 UI

--- app/src/main/java/com/teumteum/teumteum/presentation/mypage/SettingMeetingUiModel.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/mypage/SettingMeetingUiModel.kt
@@ -9,3 +9,12 @@ data class Meeting(
     val title: String,
     val time: String
 )
+
+val SignOutList = listOf(
+    "쓰지 않는 앱이에요",
+    "모임 목적과 맞지 않은 사용자가 많아요",
+    "오류가 생겨서 쓸 수 없어요",
+    "개인정보가 불안해요",
+    "앱 사용법을 모르겠어요",
+    "기타"
+)
