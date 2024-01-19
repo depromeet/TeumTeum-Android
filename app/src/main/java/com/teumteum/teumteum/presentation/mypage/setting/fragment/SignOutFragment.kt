c914532a7ed6565ddd32677999034209dc837f89
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-01-20 05:01:22 +0900

    [fix/#45] Pr Checker 에러 해결

--- app/src/main/java/com/teumteum/teumteum/presentation/mypage/setting/fragment/SignOutFragment.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/mypage/setting/fragment/SignOutFragment.kt
@@ -2,17 +2,20 @@ package com.teumteum.teumteum.presentation.mypage.setting.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.teumteum.base.BindingFragment
 import com.teumteum.teumteum.R
 import com.teumteum.teumteum.databinding.FragmentSignoutBinding
 import com.teumteum.teumteum.presentation.mypage.setting.SettingSignOutScreen
+import com.teumteum.teumteum.presentation.mypage.setting.SettingViewModel
 
 class SignOutFragment: BindingFragment<FragmentSignoutBinding>(R.layout.fragment_signout) {
+    private val viewModel: SettingViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.composeSignout.setContent {
-            SettingSignOutScreen()
+            SettingSignOutScreen(viewModel)
         }
 
     }
