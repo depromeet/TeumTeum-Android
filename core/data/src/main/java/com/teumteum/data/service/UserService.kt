c8bc1024110235cf2e77b0a5a1a7aa77f248fb36
Leekangmin <kkk5474096@naver.com>
2024-01-22 18:31:14 +0900

    [feat/#72] 유저 조회 & 유저 리스트 조회 interface

--- core/data/src/main/java/com/teumteum/data/service/UserService.kt
+++ core/data/src/main/java/com/teumteum/data/service/UserService.kt
@@ -4,11 +4,13 @@ import com.teumteum.data.model.request.RequestUserInfoWithOAuthId
 import com.teumteum.domain.entity.Friend
 import com.teumteum.domain.entity.SignUpResult
 import com.teumteum.domain.entity.UserInfo
+import com.teumteum.domain.entity.Users
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface UserService {
     @GET("users/me")
@@ -23,5 +25,15 @@ interface UserService {
     suspend fun getUserFriends(
         @Path("userId") userId: Long
     ): List<Friend>
+
+    @GET("users/{userId}")
+    suspend fun getUser(
+        @Path("userId") userId: Long
+    ): Friend
+
+    @GET("users")
+    suspend fun getUsers(
+        @Query("id") id: String
+    ): Users
 }
 
