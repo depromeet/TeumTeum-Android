c854b7d2b961c368baa37b06f832715ba0102f37
Leekangmin <kkk5474096@naver.com>
2024-01-24 16:41:26 +0900

    [feat/#83] 참여중인 사용자 클릭 시 정보로 이동

--- app/src/main/java/com/teumteum/teumteum/presentation/MainActivity.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.teumteum.teumteum.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.teumteum.base.BindingActivity
+import com.teumteum.base.util.extension.intExtra
 import com.teumteum.teumteum.R
 import com.teumteum.teumteum.databinding.ActivityMainBinding
+import com.teumteum.teumteum.presentation.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val id by intExtra()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,10 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl_main) as NavHostFragment
         val navController = navHostFragment.navController
         binding.btmNavi.setupWithNavController(navController)
+
+        if (id != -1) {
+            moveRecommendDetail()
+        }
     }
 
     fun hideBottomNavi() {
@@ -29,5 +37,18 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         binding.btmNavi.visibility = View.VISIBLE
     }
 
-    companion object {}
+    private fun moveRecommendDetail() {
+        val action =
+            HomeFragmentDirections.actionFragmentHomeToFragmentRecommendDetail(
+                id, true
+            )
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl_main) as NavHostFragment
+        navHostFragment.navController.navigate(action)
+    }
+
+    companion object {
+        fun getIntent(context: Context, id: Int) = Intent(context, MainActivity::class.java).apply {
+            putExtra("id", id)
+        }
+    }
 }
