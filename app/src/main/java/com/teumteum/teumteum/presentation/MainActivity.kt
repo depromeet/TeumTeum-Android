810a648536ab501da283f1d1eca70779e6644be7
blueme0 <ksytpdus0304@gmail.com>
2024-01-30 11:32:02 +0900

    [feat/#114] 푸시알람 pending intent 처리

--- app/src/main/java/com/teumteum/teumteum/presentation/MainActivity.kt
+++ app/src/main/java/com/teumteum/teumteum/presentation/MainActivity.kt
@@ -2,25 +2,31 @@ package com.teumteum.teumteum.presentation
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.teumteum.base.BindingActivity
+import com.teumteum.base.util.extension.boolExtra
 import com.teumteum.base.util.extension.intExtra
 import com.teumteum.teumteum.R
 import com.teumteum.teumteum.databinding.ActivityMainBinding
 import com.teumteum.teumteum.presentation.home.HomeFragmentDirections
-import com.teumteum.teumteum.util.callback.CustomBackPressedCallback
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val id by intExtra()
+    private val isFromAlarm by boolExtra()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestNotificationPermission()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl_main) as NavHostFragment
         val navController = navHostFragment.navController
@@ -28,6 +34,12 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
         if (id != -1) {
             moveRecommendDetail()
+        }
+
+        if (isFromAlarm) {
+            val action = HomeFragmentDirections.actionHomeFragmentToFragmentFamiliar()
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl_main) as NavHostFragment
+            navHostFragment.navController.navigate(action)
         }
     }
 
@@ -48,14 +60,46 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         navHostFragment.navController.navigate(action)
     }
 
+    private val requestNotificationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { ok ->
+            if (ok) {
+                // 알림권한 허용 o
+            } else {
+                // 알림권한 허용 x. 자유롭게 대응..
+            }
+        }
+
+    fun requestNotificationPermission() {
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                // 다른 런타임 퍼미션이랑 비슷한 과정
+                if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 왜 알림을 허용해야하는지 유저에게 알려주기를 권장
+                } else {
+                    requestNotificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                // 안드로이드 12 이하는 알림에 런타임 퍼미션 없으니, 설정가서 켜보라고 권해볼 수 있겠다.
+            }
+        }
+    }
+
     override fun finish() {
         super.finish()
         closeActivitySlideAnimation()
     }
 
     companion object {
-        fun getIntent(context: Context, id: Int) = Intent(context, MainActivity::class.java).apply {
+        fun getIntent(context: Context, id: Int, isFromAlarm: Boolean = false) = Intent(context, MainActivity::class.java).apply {
             putExtra("id", id)
+            putExtra("isFromAlarm", isFromAlarm)
         }
     }
 }
