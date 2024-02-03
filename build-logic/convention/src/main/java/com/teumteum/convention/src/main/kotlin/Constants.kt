7cb51c407c21817739af62c603b23a38473633c2
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-02-03 12:30:34 +0900

    [feat/#118] constants 수정

--- build-logic/convention/src/main/java/com/teumteum/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/java/com/teumteum/convention/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 40
-    const val versionName = "1.5"
+    const val versionCode = 41
+    const val versionName = "1.6"
     const val jvmVersion = "17"
 }
