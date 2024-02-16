cb8eb3cc9013084341e94dae6785ea75281d7ed8
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-02-16 14:30:15 +0900

    [fix/#158] heart icon 수정

--- app/src/main/java/com/teumteum/teumteum/config/TeumMessagingService.kt
+++ app/src/main/java/com/teumteum/teumteum/config/TeumMessagingService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
